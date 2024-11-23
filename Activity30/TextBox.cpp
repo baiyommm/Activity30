7b173728a7dee1e35c3bbc602fac23a85e620fa9
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-11-22 17:27:14 -0800

    Completed Program with 3 files and using namespace. Has constructor that has default empty string, uses getter/setter to manipulate the value in private. Has a method to demonstrate the use of explicit constructor.

---
+++ Activity30/TextBox.cpp
@@ -0,0 +1,8 @@
+//
+//  TextBox.cpp
+//  Activity30
+//
+//  Created by Chatchapong Thuaypha on 11/22/24.
+//
+
+#include "TextBox.hpp"
---
+++ Activity30/TextBox.hpp
@@ -0,0 +1,13 @@
+//
+//  TextBox.hpp
+//  Activity30
+//
+//  Created by Chatchapong Thuaypha on 11/22/24.
+//
+
+#ifndef TextBox_hpp
+#define TextBox_hpp
+
+#include <stdio.h>
+
+#endif /* TextBox_hpp */
